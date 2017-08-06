4483fbe700a276d8a3ffdb638d7c5f6688739b22
Michael Bishop <mbtyke@gmail.com>
2017-08-06 16:43:28 -0400

    Added URL format validation.

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,10 @@
 class Entry < ApplicationRecord
+  validates :url, presence: true, uniqueness: true
+  validate :valid_url?, on: :create
+
+  def valid_url?
+    URI(url)
+  rescue URI::InvalidURIError
+    errors.add(:url, "is not valid")
+  end
 end
