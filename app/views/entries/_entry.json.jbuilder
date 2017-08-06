3c43fb8d63ee0f8bf35fba3c425f3fee4adc91d4
Michael Bishop <mbtyke@gmail.com>
2017-08-06 07:43:43 -0400

    Added entries

---
+++ app/views/entries/_entry.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! entry, :id, :url, :created_at, :updated_at
+json.url entry_url(entry, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get '/', to: redirect('entries/new')
+
+  resources :entries
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/:id', to: redirect('/entries/%{id}')
 end
